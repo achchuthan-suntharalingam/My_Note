7efd4114ba37115f07c8d8bad58a4c9fa230b5bb
achchuthan suntharalingam <achchu0103@gmail.com>
2023-08-08 19:47:50 +0530

    corsOption

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const connectDB = require('./config/dbConfig');
+const errorHandler = require('./middleware/errorHandler');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const PORT = process.env.PORT || 8000
 
 app.use(express.json());
+app.use(cors(corsOptions));
+app.use(cors());
+app.use(errorHandler);
 
 //routing
 app.use('/users', require('./routes/userRoutes'));
